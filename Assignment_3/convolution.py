e8c0b297f850d99660e5054becbdc9cf5fd72309
Theresa Johansson <therjo@kth.se>
2026-04-16 16:27:42 +0200

    exercise 1 done

--- Assignment_3/convolution.py
+++ Assignment_3/convolution.py
@@ -18,7 +18,7 @@ def SlowConv(X_ims, Fs):
 
     out_h = h // f
     out_w = w // f
-    conv_out = np.zeros((out_h, out_w, nf, n), dtype=np.float64)
+    conv_out = np.zeros((out_h, out_w, nf, n), dtype=X_ims.dtype)
 
     # for each image
     for i in range(n):
@@ -37,3 +37,70 @@ def SlowConv(X_ims, Fs):
                     # apply k:th filter to patch
                     conv_out[r, c, k, i] = np.sum(patch * Fs[:, :, :, k])
     return conv_out
+
+
+def BuildMX(X_ims, f):
+    """
+    Builds matrix MX for convolution of image data.
+
+    Args:
+        X_ims: image data (32, 32, 3, n)
+        f: filter width
+    Returns:
+        MX: (n_p, f*f*3, n)
+            n_p = 64
+    """
+
+    h, w, _, n = X_ims.shape
+    out_h = h // f
+    out_w = w // f
+    n_p = out_h*out_w # num of patches
+
+    # allocate space for MX
+    MX = np.zeros((n_p, f*f*3, n), dtype=X_ims.dtype)
+
+    # for each image
+    for i in range(n):
+        # for each patch, flatten it into a row in MX
+        l = 0   # row num in MX
+        for r in range(out_h): # patch rows
+            for c in range(out_w): # patch columns
+                r0 = r*f
+                c0 = c*f
+                X_patch = X_ims[r0:r0+f, c0:c0+f,:,i]
+
+                MX[l, :, i] = X_patch.reshape((1, f*f*3), order='C')
+                
+                l += 1
+
+    return MX
+
+
+
+def FlattenFilters(Fs):
+    """
+    Args:
+        Fs: unflattened filters (f, f, 3, nf)
+    Returns:
+        Fs_flat: flattened filters (f*f*3, nf)
+    """
+    f, _, _, nf = Fs.shape
+    Fs_flat = Fs.reshape((f*f*3, nf), order='C')    
+    return Fs_flat
+
+
+
+def Conv(MX, Fs_flat):
+    """
+    Performs fast (matrix) convolution.
+
+    Args:
+        MX: (n_p, f*f*3, n)
+        Fs_flat: filters flattened (f*f*3, nf)
+    Returns:
+        conv_outputs_mat: convolution result
+                          (n_p, nf, n)
+    """
+    conv_outputs_mat = np.einsum('ijn, jl ->iln', MX, Fs_flat, optimize=True)
+    
+    return conv_outputs_mat
--- Assignment_3/debug.py
+++ Assignment_3/debug.py
@@ -8,8 +8,29 @@ from convolution import *
 
 # ---------- Slow convolution
 
-X_ims, Fs, conv_out_true = LoadDebugData()
-conv_out = SlowConv(X_ims, Fs)
+X_ims, debug_data = LoadDebugData()
+Fs = debug_data['Fs']
+conv_out_true = debug_data['conv_outputs']
+conv_out_slow = SlowConv(X_ims, Fs)
 
-print("max abs diff: ", np.max(np.abs(conv_out - conv_out_true)))
+print("slow conv_out diff: ", np.max(np.abs(conv_out_slow - conv_out_true)))
+
+
+# --------- Fast convolution
+
+f = Fs.shape[0]
+MX = BuildMX(X_ims, f)
+Fs_flat = FlattenFilters(Fs)
+
+conv_out_mat = Conv(MX, Fs_flat)
+
+n_p, _, n = MX.shape
+nf = Fs_flat.shape[1]
+
+conv_out_slow_flat =  conv_out_slow.reshape((n_p, nf, n), order='C')
+
+print("MX diff: ", np.max(np.abs(MX-debug_data['MX'])))
+print("Fs_flat diff: ", np.max(np.abs(Fs_flat - debug_data['Fs_flat'])))
+print("conv_outputs_mat diff: ", np.max(np.abs(conv_out_mat-debug_data['conv_outputs_mat'])))
+print("conv slow vs fast diff: ", np.max(np.abs(conv_out_mat-conv_out_slow_flat)))
 
